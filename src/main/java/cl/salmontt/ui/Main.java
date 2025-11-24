fd3a81083657a962cf7d93fedb564017f455ef4c
Daniel Caballero Salas <dcs.1164@hotmail.com>
2025-11-24 20:15:09 -0300

    Actualización de Main.java
    Reduccion de lineas de codigo con foreach()

---
+++ src/main/java/cl/salmontt/ui/Main.java
@@ -0,0 +1,77 @@
+package cl.salmontt.ui;
+
+import cl.salmontt.data.GestorDatos;
+import cl.salmontt.model.CentroCultivo;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Clase principal de la aplicación SalmonttApp.
+ * <p>
+ * Esta clase orquesta la lógica del programa, incluyendo la creación de
+ * datos iniciales, la escritura de estos datos en el archivo CSV a través
+ * del {@code GestorDatos}, la lectura posterior y el filtrado de la información.
+ * </p>
+ * @author [Daniel Caballero Salas]
+ * @version 1.0
+ */
+public class Main {
+    private Main(){} // <- solo para arreglar el formato de Javadoc
+    public static void main(String[] args) {
+        /**
+         * Metodo principal que se ejecuta al iniciar la aplicación.
+         * <p>
+         * Se encarga de:
+         * 1. Inicializar una lista de {@code CentroCultivo} con datos de prueba.
+         * 2. Llamar a {@code GestorDatos} para guardar la lista en un archivo CSV.
+         * 3. Llamar a {@code GestorDatos} para cargar los datos desde el archivo.
+         * 4. Mostrar todos los centros
+         * 5. Filtrar y mostrar los centros con una producción superior a 1000 toneladas.
+         * </p>
+         * @param args Argumentos de la línea de comandos (no utilizados en este caso).
+         */
+
+        //Se rear una lista vacia que guardará los centros de cultivo
+        List<CentroCultivo> ListadoCentros = new ArrayList<>();
+
+        //En la lista agregar cada producto con el contructor de la clase CentroCultivo
+        ListadoCentros.add(new CentroCultivo("Isla Huar","Calbuco",1200));
+        ListadoCentros.add(new CentroCultivo("Isla Puluqui","Calbuco",700));
+        ListadoCentros.add(new CentroCultivo("Canal Tenglo","Puerto Montt",1350));
+        ListadoCentros.add(new CentroCultivo("Bahía Ilque","Puerto Varas",1620));
+        ListadoCentros.add(new CentroCultivo("Canal Dalcahue","Dalcahue",880));
+
+        //Se crea el archivo con el nombre:
+        String nombreArchivo = "centros.csv";
+
+        //Se llama al metodo agregarDatos en GestorDatos
+        GestorDatos gd = new GestorDatos();
+
+        //Se scriben en el archivo .CSV los datos ingresados en ListadCentros
+        gd.agregarDatos(ListadoCentros, nombreArchivo);
+
+        //------------------------------------------------------------------
+
+        // --- Cargar datos desde archivo .CSV ---
+
+        //Lista que almacena los centros obtenidos de GestorDatos como objeto
+        ArrayList<CentroCultivo> centros = gd.cargarCentros();
+
+        //Se extraen los datos
+        System.out.println("\n...::: Todos los centros :::...");
+        //Se imprimen en pantalla todos los datos
+        centros.forEach(System.out::println);
+
+        // --- Filtro para toneladas > 1000 ---
+
+        // Titulo del filtro
+        System.out.println("\n...::: Centros con producción > 1000 toneladas :::...");
+        centros.stream()
+                //Se aplica el filtro
+                .filter(centro -> centro.getToneladas() > 1000)
+                //Se muestran los resultados filtrados
+                .forEach(System.out::println);
+
+    }
+
+}
